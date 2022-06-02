8558877972d12f68ae2d37fb8e9caffdd66023a0
Julien1793 <105311321+Julien1793@users.noreply.github.com>
2022-06-02 15:53:32 +0200

    Update main.js

--- main.js
+++ main.js
@@ -100,6 +100,22 @@ function init(){
             featureLayer.clearLayers();
         }
     }
+    
+    /*Legend specific*/
+    var legend = L.control({ position: "bottomleft" });
+
+    legend.onAdd = function(map) {
+    var div = L.DomUtil.create("div", "legend");
+    div.innerHTML += "<h4>Legende</h4>";
+    div.innerHTML += '<i style="border: solid #FF0000;background: #F2A0A0"></i><span>Parcelle (IGN)</span><br>';
+    //div.innerHTML += '<i style="background: #448D40"></i><span>Forest</span><br>';
+    //div.innerHTML += '<i style="background: #E6E696"></i><span>Land</span><br>';
+    //div.innerHTML += '<i style="background: #E8E6E0"></i><span>Residential</span><br>';
+    //div.innerHTML += '<i style="background: #FFFFFF"></i><span>Ice</span><br>';
+    //div.innerHTML += '<i class="icon" style="background-image: url(https://d30y9cdsu7xlg0.cloudfront.net/png/194515-200.png);background-repeat: no-repeat;"></i><span>Grænse</span><br>';
+    return div;
+    };
+    legend.addTo(map);
 
  
 }
